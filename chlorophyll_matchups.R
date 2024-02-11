182e4ac582cab4703d89a8a9730abec64489a89d
rrcarlson <Rachel.R.Carl@gmail.com>
2024-02-11 13:39:10 -0800

    Changes to filtering by distance and depth.

--- chlorophyll_matchups.R
+++ chlorophyll_matchups.R
@@ -9,9 +9,9 @@ library(lubridate)
 library(ggplot2)
 
 ##### Read in MOCHA data and filter for points with chlorophyll data
-oa <- clean_all
-oa$date <- as.Date(oa$time_utc) %>% format("%y-%m-%d")
-chl <- oa %>% filter(!is.na(chl_ugL)) %>% filter(depth_m <= 10) # Filter for points where chlorophyll exists, and at a depth <= 15 m (mixed layer)
+oa <- clean_all %>% filter(depth_m <=10) %>% filter(distance_offshore <= 10) # Filter for points where chlorophyll exists, and at a depth <= 15 m (mixed layer) and within 10 km of shore
+oa$date <- as.Date(oa$time_utc) %>% format("%Y-%m-%d")
+chl <- oa %>% filter(!is.na(chl_ugL))
 
 # Depth-specific chlorophyll means for site-days
 chl_per_depth <- chl %>% group_by(dataset_id, latitude, longitude, depth_m, date) %>% summarize(
@@ -21,8 +21,6 @@ chl_per_depth <- chl %>% group_by(dataset_id, latitude, longitude, depth_m, date
 chl_mixed <- chl %>% group_by(dataset_id, latitude, longitude, date) %>% summarize(
   chl = mean(chl_ugL, na.rm = TRUE))
 
-write.csv(chl_mixed, '/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/RS_chlorophyll/MOCHA_matchups.csv')
+write.csv(chl_mixed, '/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/RS_chlorophyll/Data/MOCHA_matchups.csv')
 
-sf_chl_mixed <- st_as_sf(chl_mixed, coords = c("longitude", "latitude"))
-st_write(sf_chl_mixed, "/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/RS_chlorophyll/Data/MOCHA_matchups.shp")
 
