c8ea85e9fd6f41724cdaa214df6a973ee5df2af9
rrcarlson <Rachel.R.Carl@gmail.com>
2024-02-14 17:58:23 -0800

    Filter OA dataset for points where pCO2 or combined alk/pH data exist (OA parameters).

--- chlorophyll_matchups.R
+++ chlorophyll_matchups.R
@@ -9,7 +9,7 @@ library(lubridate)
 library(ggplot2)
 
 ##### Read in MOCHA data and filter for points with chlorophyll data
-oa <- clean_all %>% filter(depth_m <=10) %>% filter(distance_offshore <= 10) # Filter for points where chlorophyll exists, and at a depth <= 15 m (mixed layer) and within 10 km of shore
+oa <- clean_all %>% filter(depth_m <=15) %>% filter(distance_offshore <= 50) # Filter for points where chlorophyll exists, and at a depth <= 15 m (mixed layer) and within 10 km of shore
 oa$date <- as.Date(oa$time_utc) %>% format("%Y-%m-%d")
 oa$year <- year(oa$date)
 chl <- oa %>% filter(!is.na(chl_ugL))
@@ -27,4 +27,19 @@ chl_sentinel <- chl_mixed %>% filter(date > '2019-06-08')
 
 write.csv(chl_mixed, '/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/RS_chlorophyll/Data/MOCHA_matchups.csv')
 write.csv(chl_sentinel, '/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/RS_chlorophyll/Data/MOCHA_matchups_sentinel.csv')
+
+pco <- oa %>% filter(!is.na(pCO2_uatm) | (!is.na(pH_total & !is.na(ta_umolkg))))
+pco_sum <- pco %>% group_by(dataset_id, latitude, longitude, year, date) %>% summarize(
+  t = mean(t_C, na.rm = TRUE),
+  sal = mean(sal_pss, na.rm = TRUE),
+  ta = mean(ta_umolkg, na.rm = TRUE),
+  ph = mean(pH_total, na.rm = TRUE),
+  alk = mean(ta_umolkg, na.rm = TRUE),
+  do = mean(do_umolkg, na.rm = TRUE),
+  pco2 = mean(pCO2_uatm, na.rm = TRUE),
+  chl = mean(chl_ugL, na.rm = TRUE)
+  )
+pco_recent <- pco_sum %>% filter(date > '2019-06-08')
+
+
 
